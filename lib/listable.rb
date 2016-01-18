fd417fa7124989171f23ef6a5b9a9e56bd4870c5
Karim Tarek <karimmtarek@gmail.com>
2016-01-17 21:44:10 -0500

    Add format_date method ot Listable module

--- lib/listable.rb
+++ lib/listable.rb
@@ -3,12 +3,19 @@ module Listable
     "#{description}".ljust(30)
   end
 
+  def format_date(start_or_due_date: nil, end_date: nil)
+    dates = start_or_due_date.strftime("%D") if start_or_due_date
+    dates << " -- " + end_date.strftime("%D") if end_date
+    dates = "N/A" unless dates
+    dates
+  end
+
   def format_priority(priority)
     value = " ⇧" if priority == "high"
     value = " ⇨" if priority == "medium"
     value = " ⇩" if priority == "low"
     value = "" unless priority
 
-    return value
+    value
   end
 end
--- lib/todo.rb
+++ lib/todo.rb
@@ -7,12 +7,10 @@ class TodoItem
     @due = options[:due] ? Date.parse(options[:due]) : options[:due]
     @priority = options[:priority]
   end
-  def format_date
-    @due ? @due.strftime("%D") : "No due date"
-  end
+
   def details
     format_description(description) + "due: " +
-    format_date +
+    format_date(start_or_due_date: due) +
     format_priority(priority)
   end
 end
